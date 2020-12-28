8f328b5331ba0bc69974b8b6e510536022aad687
Koji Sakamoto <sakamoto5002@gmail.com>
2020-12-28 13:14:21 +0900

    Topページを表示

--- src/main/java/skmt/com/officeutq/SkmtApplication.java
+++ src/main/java/skmt/com/officeutq/SkmtApplication.java
@@ -2,28 +2,28 @@ package skmt.com.officeutq;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
+/**
+ * Skmt Topページ
+ * @author Koji Sakamoto
+ *
+ */
 @SpringBootApplication
-@RestController
+@Controller
 public class SkmtApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SkmtApplication.class, args);
 	}
 	
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(SkmtApplication.class);
+    @RequestMapping(value = "/top", method = RequestMethod.GET)
+    public String getSomething(Model model) {
+		model.addAttribute("top",null);
+		return "top";
     }
-
-    @RequestMapping(value = "nagap", method = RequestMethod.GET)
-    public String getSomething() {
-        return "something";
-    }
-
 }
