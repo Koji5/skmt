52d13939f0ac593b20cc1f69e8bf0cf8df1b5bec
Koji Sakamoto <sakamoto5002@gmail.com>
2020-12-30 18:34:20 +0900

    Thymeleaf追加

---
+++ src/main/java/skmt/com/officeutq/config/ThymeleafConfig.java
@@ -0,0 +1,43 @@
+package skmt.com.officeutq.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.thymeleaf.extras.springsecurity5.dialect.SpringSecurityDialect;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+
+import nz.net.ultraq.thymeleaf.LayoutDialect;
+
+@Configuration
+public class ThymeleafConfig {
+	
+	@Bean
+	public SpringResourceTemplateResolver templateResolver() {
+		SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
+		resolver.setPrefix("/WEB-INF/templates/");
+		resolver.setSuffix(".html");
+		resolver.setTemplateMode("HTML5");
+		resolver.setCharacterEncoding("UTF-8");
+		return resolver;
+	}
+
+	@Bean
+	public SpringTemplateEngine templateEngine() {
+		SpringTemplateEngine engine = new SpringTemplateEngine();
+		engine.addDialect(new LayoutDialect());
+		engine.addDialect(new SpringSecurityDialect());
+		engine.setTemplateResolver(templateResolver());
+		return engine;
+	}
+	
+	@Bean
+	public ThymeleafViewResolver thymeleafViewResolver() {
+		ThymeleafViewResolver resolver = new ThymeleafViewResolver();
+		resolver.setTemplateEngine(templateEngine());
+		resolver.setCharacterEncoding("UTF-8");
+		resolver.setOrder(1);
+		return resolver;
+	}
+
+}
